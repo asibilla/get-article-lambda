4c583885cb37473452df194b403870b902dbf101
Andrew Sibilla <mr.andy.sibilla@gmail.com>
2026-05-06 16:38:49 -0700

    chore: add codebuild template

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import { APIGatewayEvent } from 'aws-lambda'
 
 export const lambdaHandler = (event: APIGatewayEvent) => {
-    console.log('hi from lambda!', event)
+    console.log('hi from lambda!', event);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: "Hello World",
+        }),
+    };
+
+    return response;
 }
 
